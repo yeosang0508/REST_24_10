c30e96a6f032b1e6561b8385afdddc5d3f9bf162
yeosang0508 <yeosang0508@gmail.com>
2024-10-29 13:28:10 +0900

    프로젝트 세팅

---
+++ src/test/java/com/koreait/rest_24_10/Rest2410ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.rest_24_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rest2410ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
